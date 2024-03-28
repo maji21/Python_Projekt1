dd42d4492a7cfe1357706f0527c16837430a5318
makan002 <mariam.kandji002@stud.fh-dortmund.de>
2024-03-28 18:29:08 +0100

    Slot Maschine komlett + Fehler behoben

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import random
-MAX_LINES = 3 #globale Konstanze
+
+MAX_LINES = 3 #Global
 MAX_WETTE = 100
 MIN_WETTE = 1
 
-ZEIlE = 3
+ZEILE = 3
 SPALTE = 3
+
 symbole_counter = {
     "A": 2,
     "B": 4,
@@ -12,47 +14,71 @@ symbole_counter = {
     "D": 8
 }
 
+symbole_werte = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
+def check_gewinne(spalten,lines,wetten,werte):
+  gewinne = 0
+  gewinne_lines = []
+  for line in range(lines):
+      symbol = spalten[0][line]
+      for spalte in spalten:
+          symbol_check = spalte[line]
+          if symbol != symbol_check:
+              break
+          else:
+              gewinne += werte[symbol] * wetten
+              gewinne_lines.append(line + 1)
+
+              return gewinne, gewinne_lines
+
+
+
+
 def get_solt_maschine_drehen(zeilen, spalte, symbole):
     alle_symbole = []
     for symbol, symbole_counter in symbole.items():
         for _ in range(symbole_counter): # Anonyme Variable
             alle_symbole.append(symbol)
+
             spalten = []
             for _ in range(spalte):
-                spalte = []                       #zzufällige Werte für jede zeile
+                spalte = []                       #zufällige Werte für jede zeile
                 aktuelle_symbole = alle_symbole [:] # Kopie
                 for _ in range(zeilen):
                     wert = random.choice(aktuelle_symbole)
                     aktuelle_symbole.remove(wert)
+
                     spalte.append(wert)
 
                     spalten.append(spalte)
 
                     return spalten
 
-
 def print_slot_maschine(spalten):
     for zeile in range(len(spalten[0])): # min eine Spalte
-        for spalte in spalten:
             for i, spalte in enumerate(spalten):
                 if i != len(spalten) - 1:
-                    print(spalte[zeile], "|")
+                    print(spalte[zeile], end=" | ")
                 else:
-                    print(spalte[zeile])
+                    print(spalte[zeile], end=" ")
 
-
-
-
+            print()
 
 
 def einzahlung():
     while True: #Benutzer gibt so lange Menge ein bis gültig ist
         betrag = input("Wie viel Geld wollen Sie einzahlen? €")
-        #Prüfen, ob Betrag eine Nummer ist
-        if betrag.isdigit():
+        if betrag.isdigit():  #Prüfen, ob Betrag eine Nummer ist
             betrag = int(betrag) #Betrag in Zahl umgewandelt
             if betrag > 0:
-                break
+              #  break
+             return betrag
             else:
                 print("Der Betrag muss größer als 0 sein")
         else:
@@ -60,7 +86,7 @@ def einzahlung():
 
             return betrag
 
-#1. Fragen wie oft und wie viel Wetten wollen
+# Fragen wie oft und wie viel Wetten wollen
 def get_anzahl_der_lines():
     while True: #Benutzer gibt so lange Menge ein bis gültig ist
         lines = input("Wie oft wollen Sie Wetten? (1 - " + str(MAX_LINES) + ") ?")
@@ -68,7 +94,8 @@ def get_anzahl_der_lines():
         if lines.isdigit():
             lines = int(lines) #Betrag in Zahl umgewandelt
             if 1 <= lines <= MAX_LINES:
-                break
+               # break
+                 return lines
             else:
                 print("Bitte geben Sie Zahl im gültigen Wertebereich ein")
         else:
@@ -76,36 +103,52 @@ def get_anzahl_der_lines():
 
             return lines
 
-
-
 def get_wette():
     while True:
-        betrag = input("Wie viel Geld wollen Sie pro runde Wetten? €")
+        betrag = input("Wie viel wollen Sie auf jede Linie setzen ?")
         if betrag.isdigit():
-            betrag = int(betrag)  # Betrag in Zahl umgewandelt
+            betrag = int(betrag)
             if MIN_WETTE <= betrag <= MAX_WETTE:
-                break
+                return betrag
             else:
-                print(f"Der Betrag muss zwischen sein €{MIN_WETTE} - €{MAX_WETTE} sein.")
+                print(f"Betrag muss zwischen € {MIN_WETTE} - € {MAX_WETTE} sein.")
 
-        else:
-            print("Bitte geben Sie eine gültigen Zahl ein")
+ #           return betrag
 
-            return betrag
+
+
+
+
+
+def spiel(guthaben):
+            lines = get_anzahl_der_lines()
+            while True:
+                wette = get_wette()
+                gesamt_wette =  wette * lines
+
+                if gesamt_wette > guthaben:
+                    print(f"Sie haben nicht genug Guthaben, Ihr aktuelles Guthabe beträgt € {guthaben}")
+                else:
+                    break
+
+            print(f"Sie wetten:{wette} mit {lines} Versuchen. Gesamte Wette ist €{gesamt_wette}")
+
+            slots = get_solt_maschine_drehen(ZEILE, SPALTE, symbole_counter)
+            print_slot_maschine(slots)
+            gewinne, gewinne_lines = check_gewinne(slots, lines, wette, symbole_werte)
+            print(f"Sie haben €{gewinne} gewonnen")
+            print(f"You won on lines:", *gewinne_lines)
+            return gewinne - gesamt_wette
+
+
 def main():
    guthaben = einzahlung()
-   lines = get_anzahl_der_lines()
    while True:
-       wette = get_wette()
-       gesamt_wette = wette * lines
-
-       if gesamt_wette > guthaben:
-           print(f"Sie haben nicht genug Guthaben, Ihr aktuelles Guthabe beträgt € {guthaben}")
-       else:
+       print(f"Ihr aktuelles Guthaben beträgt: €{guthaben}")
+       antwort = input("Drücke auf Enter zum Drehen. (b für beenden)")
+       if antwort == "b":
            break
+       guthaben += spiel(guthaben)
+       print((f"Sie haben noch €{guthaben} "))
 
-
-
-       print(f"Sie wetten:{wette} mit {lines} Versuchen. Gesamte Wette ist €{gesamt_wette}")
 main()
-
